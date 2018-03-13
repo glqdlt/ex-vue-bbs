642fbaae23c11bde5545b22c43ed362eb1084078
glqdlt <dlfdnd0725@gmail.com>
2018-03-13 16:13:13 +0900

    간단한 tag 와 wysiwyg 를 구현

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <!--https://github.com/matiastucci/vue-input-tag-->
+    <div>
+      <input-tag :tags.sync="myTags"></input-tag>
+    </div>
+
+    <!--https://github.com/chmln/vue-wysiwyg-->
+    <wysiwyg v-model="myHTML" />
+    <pre>{{$data}}</pre>
   </div>
+
 </template>
 
 <script>
@@ -24,13 +17,17 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      myHTML : '',
+      myTags : []
     }
   }
 }
 </script>
 
 <style>
+  @import "~vue-wysiwyg/dist/vueWysiwyg.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import wysiwyg from "vue-wysiwyg"
+import InputTag from 'vue-input-tag'
+
+
+Vue.use(wysiwyg, {});
+Vue.component('input-tag', InputTag);
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
